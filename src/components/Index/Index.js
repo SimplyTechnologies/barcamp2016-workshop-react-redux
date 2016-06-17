9ea7011c3cf3b3e86f2b6303b9a6923761ba7430
Michael Manukyan <arm.localhost@gmail.com>
2016-06-17 12:44:18 +0400

    remove components

--- src/components/Index/Index.js
+++ src/components/Index/Index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 
-import { ActionBar, Location, WeatherList } from 'components';
-
 export default function Index() {
   return (
     <div className="container">
-      <Location />
-      <ActionBar />
-      <WeatherList />
+      "Hi. BarCamp"
     </div>
   );
 }
--- src/redux/modules/sagas.js
+++ src/redux/modules/sagas.js
@@ -1,12 +1,4 @@
-import { fork } from 'redux-saga/effects';
-import { watchWeatherLoad } from './weather.saga';
-import { watchLocationGet } from './location.saga';
-import { watchForecastLoad } from './forecast.saga';
-
 export default function* sagas() {
   yield [
-    fork(watchWeatherLoad),
-    fork(watchLocationGet),
-    fork(watchForecastLoad),
   ];
 }
